8d0e4aa1cfdc05b0fca10213aca9c1989db84166
ZaidYounan <zaid.younan@gmail.com>
2021-07-15 23:03:45 +1000

    Create navbar, implement basic routes, import google font, adjust styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,63 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        LOGO <i className='fab fa-typo3'/>
+                        FIGHT CLUB <i className='fas fa-bolt'/>
                     </Link>
-                    <div className="menu-icon">
+                    <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times': 'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/fighters' className='nav-links' onClick={closeMobileMenu}>
+                                Boxers
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/schedule' className='nav-links' onClick={closeMobileMenu}>
+                                Fight Schedule
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+                                Contact Us
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>COACH SIGN UP</Button>}
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
