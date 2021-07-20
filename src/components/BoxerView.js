bd86a7f4306884383a0b0733359cc5c4255e2291
ZaidYounan <zaid.younan@gmail.com>
2021-07-21 02:02:39 +1000

    Fetch gym resources with axios and map gym Array in order to print gym name on BoxerView

--- src/components/BoxerView.js
+++ src/components/BoxerView.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 function BoxerView() {
     const [boxers, setBoxers] = useState([]);
+    const [gyms, setGyms] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:3001/boxers/')
@@ -16,24 +17,49 @@ function BoxerView() {
             })
     }, []);
 
+    useEffect(() => {
+        axios.get('http://localhost:3001/gyms/')
+            .then(response => {
+                setGyms(response.data);
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, []);
+
+
+
     return (
         boxers ? (
-        <>
+
+
+            
+            
             <>
+
+            
                 {boxers.map((data) => (
                     <div class="boxer-container"> 
                         <td>Name: {data.first_name} {data.last_name}</td>           
                         <td>Height: {data.height}cm</td>
                         <td>Weight: {data.weight}kg</td>
-                        <td>Gym ID: {data.gym_id}</td>
+                            { gyms.map((gym) => {
+                                if (gym.id == data.gym_id) {
+                                    return <div>Gym: {gym.name}</div>
+                                } else {
+                                return
+                            }
+                        })}
                     </div>
                 ))}
             </>
-        </>
     ) : ( 
         <div>Loading Boxers</div> 
         )
     );
 }
+
+
 
 export default BoxerView;
