7f7cf808292880374f7b75e4db402e0093b47b77
ZaidYounan <zaid.younan@gmail.com>
2021-07-21 00:26:12 +1000

    Connect and display BoxerView with Boxer data from API back-end

--- src/components/BoxerView.js
+++ src/components/BoxerView.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './BoxerView.css';
+import axios from 'axios';
 
 function BoxerView() {
-    const [boxer, setBoxer] = useState([]);
+    const [boxers, setBoxers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/boxers/')
+            .then(response => {
+                setBoxers(response.data);
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, []);
+
     return (
-        <div>
-            <h1>Boxers go here</h1>
-        </div>
-    )
+        boxers ? (
+        <>
+            <>
+                {boxers.map((data) => (
+                    <div class="boxer-container"> 
+                        <td>Name: {data.first_name} {data.last_name}</td>           
+                        <td>Height: {data.height}cm</td>
+                        <td>Weight: {data.weight}kg</td>
+                        <td>Gym ID: {data.gym_id}</td>
+                    </div>
+                ))}
+            </>
+        </>
+    ) : ( 
+        <div>Loading Boxers</div> 
+        )
+    );
 }
 
 export default BoxerView;
--- src/components/ScheduleView.js
+++ src/components/ScheduleView.js
@@ -3,8 +3,6 @@ import { Button } from './Button';
 import './ScheduleView.css';
 
 function ScheduleView () {
-
-
     return (
         <div className="schedule-container">
             <Button link='/schedule/new' buttonStyle='btn--schedule'>Click to Schedule A Fight</Button>
