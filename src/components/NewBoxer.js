3eb57c4a4161cb8ecbb9bf83f9e3f8a6901dffc4
Mitchell Guthmann <79577106+mitchguthmann@users.noreply.github.com>
2021-07-23 16:12:41 +1000

    update model Coach to User, fixed routing for BE

--- src/components/NewBoxer.js
+++ src/components/NewBoxer.js
@@ -1,133 +1,159 @@
-import  { useState } from 'react';
-import './NewBoxer.css'
-import axios from 'axios';
-
+import { useState } from "react";
+import "./NewBoxer.css";
+import axios from "axios";
 
 function NewBoxer() {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [height, setHeight] = useState('');
-    const [weight, setWeight] = useState('');
-    const [reach, setReach] = useState('');
-    const [stance, setStance] = useState('');
-    const [gym, setGym] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
-    const [data, setData] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [reach, setReach] = useState("");
+  const [stance, setStance] = useState("");
+  const [gym, setGym] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState(null);
 
-    const handleSubmit = () => {
-        setLoading(true);
-        setIsError(false);
-        const data = {
-            first_name: firstName,
-            last_name: lastName,
-            height: height,
-            weight: weight,
-            reach: reach,
-            stance: stance,
-            gym_id: gym,
-        }
-        axios.post('http://localhost:3001/boxers/new', data).then(res =>{
-            setData(res.data);
-            setFirstName('');
-            setLastName('');
-            setHeight('');
-            setWeight('');
-            setReach('');
-            setStance('');
-            setGym('');
-            setLoading(false);
-        })
-    }
+  const handleSubmit = () => {
+    setLoading(true);
+    setIsError(false);
+    const data = {
+      first_name: firstName,
+      last_name: lastName,
+      height: height,
+      weight: weight,
+      reach: reach,
+      stance: stance,
+      gym_id: gym,
+    };
+    axios.post("http://localhost:3001/boxers", data).then((res) => {
+      setData(res.data);
+      setFirstName("");
+      setLastName("");
+      setHeight("");
+      setWeight("");
+      setReach("");
+      setStance("");
+      setGym("");
+      setLoading(false);
+    });
+  };
 
-    return (
-        <div className="container">
-        <div style={{ maxWidth: 350 }}>
-          <div className="form-group">
-            <label htmlFor="firstName">First Name</label>
-            <input
-              type="text"
-              className="form-control"
-              id="first_name"
-              placeholder="First Name"
-              value={firstName}
-              onChange={e => setFirstName(e.target.value)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastName" className="mt-2">Last Name</label>
-            <input
-              type="text"
-              className="form-control"
-              id="last_name"
-              placeholder="Last Name"
-              value={lastName}
-              onChange={e => setLastName(e.target.value)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="height" className="mt-2">Height</label>
-            <input
-              type="number"
-              className="form-control"
-              id="height"
-              placeholder="Height"
-              value={height}
-              onChange={e => setHeight(e.target.value)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="weight" className="mt-2">Weight</label>
-            <input
-              type="number"
-              className="form-control"
-              id="weight"
-              placeholder="Weight"
-              value={weight}
-              onChange={e => setWeight(e.target.value)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="reach" className="mt-2">Reach</label>
-            <input
-              type="number"
-              className="form-control"
-              id="reach"
-              placeholder="Reach"
-              value={reach}
-              onChange={e => setReach(e.target.value)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="stance" className="mt-2">Stance</label>
-            <input
-              type="string"
-              className="form-control"
-              id="stance"
-              placeholder="Stance"
-              value={stance}
-              onChange={e => setStance(e.target.value)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="gym" className="mt-2">Gym</label>
-            <input
-              type="number"
-              className="form-control"
-              id="gym_id"
-              placeholder="Gym ID"
-              value={gym}
-              onChange={e => setGym(e.target.value)} />
-          </div>
-          {isError && <small className="mt-3 d-inline-block text-danger">Something went wrong. Please try again later.</small>}
-          <button
-            type="submit"
-            className="btn btn-primary mt-3"
-            onClick={handleSubmit}
-            disabled={loading}
-          >{loading ? 'Loading...' : 'Submit'}</button>
-          {data && <div className="mt-3">
-            <strong>Output:</strong><br />
+  return (
+    <div className="container">
+      <div style={{ maxWidth: 350 }}>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="first_name"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName" className="mt-2">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="last_name"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="height" className="mt-2">
+            Height
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="height"
+            placeholder="Height"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="weight" className="mt-2">
+            Weight
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="weight"
+            placeholder="Weight"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="reach" className="mt-2">
+            Reach
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="reach"
+            placeholder="Reach"
+            value={reach}
+            onChange={(e) => setReach(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="stance" className="mt-2">
+            Stance
+          </label>
+          <input
+            type="string"
+            className="form-control"
+            id="stance"
+            placeholder="Stance"
+            value={stance}
+            onChange={(e) => setStance(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="gym" className="mt-2">
+            Gym
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="gym_id"
+            placeholder="Gym ID"
+            value={gym}
+            onChange={(e) => setGym(e.target.value)}
+          />
+        </div>
+        {isError && (
+          <small className="mt-3 d-inline-block text-danger">
+            Something went wrong. Please try again later.
+          </small>
+        )}
+        <button
+          type="submit"
+          className="btn btn-primary mt-3"
+          onClick={handleSubmit}
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Submit"}
+        </button>
+        {data && (
+          <div className="mt-3">
+            <strong>Output:</strong>
+            <br />
             <pre>{JSON.stringify(data, null, 2)}</pre>
           </div>
-          }
-        </div>
+        )}
       </div>
-    )
+    </div>
+  );
 }
 
-export default NewBoxer
+export default NewBoxer;
--- src/components/NewFight.js
+++ src/components/NewFight.js
@@ -1,12 +1,144 @@
-import React from 'react';
-import './NewFight.css';
+import { useState } from "react";
+import React from "react";
+import "./NewFight.css";
+import axios from "axios";
 
 function NewFight() {
-    return (
-        <div className="ff-container">
-            <h1>Form Will Go Here</h1>
+  const [boxerA, setBoxerA] = useState("");
+  const [boxerB, setBoxerB] = useState("");
+  const [user, setUser] = useState("");
+  const [timeScheduled, setTimeScheduled] = useState("");
+  const [rounds, setRounds] = useState("");
+  const [gym, setGym] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState(null);
+
+  const handleSubmit = () => {
+    setLoading(true);
+    setIsError(false);
+    const data = {
+      boxer_a: boxerA,
+      boxer_b: boxerB,
+      user_id: user,
+      time_scheduled: timeScheduled,
+      rounds: rounds,
+      gym_id: gym,
+    };
+    axios.post("http://localhost:3001/fights", data).then((res) => {
+      setData(res.data);
+      setBoxerA("");
+      setBoxerB("");
+      setCoach("");
+      setTimeScheduled("");
+      setRounds("");
+      setGym("");
+      setLoading(false);
+    });
+  };
+
+  return (
+    <div className="container">
+      <div style={{ maxWidth: 350 }}>
+        <div className="form-group">
+          <label htmlFor="boxerA">Home Boxer</label>
+          <input
+            type="text"
+            className="form-control"
+            id="boxer_a"
+            placeholder="Home Boxer"
+            value={boxerA}
+            onChange={(e) => setBoxerA(e.target.value)}
+          />
         </div>
-    )
+        <div className="form-group">
+          <label htmlFor="boxerB" className="mt-2">
+            Away Boxer
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="boxer_b"
+            placeholder="Away Boxer"
+            value={boxerB}
+            onChange={(e) => setBoxerB(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="user" className="mt-2">
+            Coach
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="user"
+            placeholder="Coach"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="timeScheduled" className="mt-2">
+            Time Scheduled
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="timeScheduled"
+            placeholder="Time Scheduled"
+            value={timeScheduled}
+            onChange={(e) => setTimeScheduled(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="rounds" className="mt-2">
+            Rounds
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="rounds"
+            placeholder="Rounds"
+            value={rounds}
+            onChange={(e) => setRounds(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="gym" className="mt-2">
+            Gym
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="gym_id"
+            placeholder="Gym ID"
+            value={gym}
+            onChange={(e) => setGym(e.target.value)}
+          />
+        </div>
+        {isError && (
+          <small className="mt-3 d-inline-block text-danger">
+            Something went wrong. Please try again later.
+          </small>
+        )}
+        <button
+          type="submit"
+          className="btn btn-primary mt-3"
+          onClick={handleSubmit}
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Submit"}
+        </button>
+        {data && (
+          <div className="mt-3">
+            <strong>Output:</strong>
+            <br />
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default NewFight
+export default NewFight;
