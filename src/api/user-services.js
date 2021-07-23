d3000db3e290119d7e21c1958ddf1701a6f568f7
ZaidYounan <zaid.younan@gmail.com>
2021-07-23 16:17:46 +1000

    Install react-validation and validator libraries, create Login and Register component including forms and basic validations

--- src/api/user-services.js
+++ src/api/user-services.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8080/api/test/";
+const API_URL = "process.env.REACT_APP_API_URL";
 
 const getUserBoard = () => {
   return axios.get(API_URL + "user", { headers: authHeader() });
 };
 
+
+// THIS COMPONENT MIGHT BE USELESS
 
 export default {
   getUserBoard,
