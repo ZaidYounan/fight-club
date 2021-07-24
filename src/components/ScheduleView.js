9b1ae52b987fcae9064278456f03fb4a072307ed
Mitchell Guthmann <79577106+mitchguthmann@users.noreply.github.com>
2021-07-24 20:55:42 +1000

    Added ScheduleView and scss

--- src/components/ScheduleView.js
+++ src/components/ScheduleView.js
@@ -1,13 +1,93 @@
-import React from 'react'
-import { Button } from './Button';
-import './ScheduleView.css';
+import React, { useEffect, useState } from "react";
+import "./ScheduleView.css";
+import axios from "axios";
+import { Button } from "./Button";
 
-function ScheduleView () {
-    return (
-        <div className="schedule-container">
-            <Button link='/schedule/new' buttonStyle='btn--schedule'>Click to Schedule A Fight</Button>
+function ScheduleView() {
+  const [fights, setFights] = useState([]);
+  const [gyms, setGyms] = useState([]);
+  const [boxers, setBoxers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/fights/")
+      .then((response) => {
+        setFights(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/boxers/")
+      .then((response) => {
+        setBoxers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/gyms/")
+      .then((response) => {
+        setGyms(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return fights ? (
+    <div className="fight-container">
+      {fights.map((data) => (
+        <div className="fight-cards" key={data.id}>
+          {boxers.map((boxer) => {
+            if (boxer.id === data.boxer_a_id) {
+              return (
+                <div>
+                  {boxer.first_name} {boxer.last_name}
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+          <div>Vs</div>
+
+          {boxers.map((boxer) => {
+            if (boxer.id === data.boxer_b_id) {
+              return (
+                <div>
+                  {boxer.first_name} {boxer.last_name}
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+
+          <td>Scheduled Date: {data.time_scheduled}</td>
+
+          {gyms.map((gym) => {
+            if (gym.id === data.gym_id) {
+              return <div>@{gym.name}</div>;
+            } else {
+              return null;
+            }
+          })}
         </div>
-    )
+      ))}
+      <Button link="/fights/new" buttonStyle="btn--schedule">
+        Schedule a New Fight
+      </Button>
+    </div>
+  ) : (
+    <div>Loading Schedule</div>
+  );
 }
 
 export default ScheduleView;
