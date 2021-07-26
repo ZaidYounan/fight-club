6ef414132b2df32f71fc6fc0bad16ea6e2267078
ZaidYounan <zaid.younan@gmail.com>
2021-07-26 19:03:17 +1000

    Edit about page and app.css styling, change .env to production URL for deployment testing

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -7,7 +7,7 @@ function AboutView() {
     <>
         <div className="image-aboutus-banner">
             <div className="col-lg-8 about-text">
-                <h1 className="lg-text">About Us</h1>
+                <h1 className="lg-text">ABOUT US</h1>
                 <h2>Fight Club is a database of local fighters, with the goal of allowing coaches to more easily find competitors of the appropriate weight class and experience to match with their fighters.</h2>
                 <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
